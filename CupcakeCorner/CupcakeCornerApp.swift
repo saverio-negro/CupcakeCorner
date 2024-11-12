d717be0970d4c3de54d1c8ab1cf58d9726f3a9c6
Saverio Negro <saverionegro@gmail.com>
2024-11-12 21:47:43 +0100

    Initial commit.

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Saverio Negro on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCorner/Notes.md
@@ -0,0 +1,7 @@
+#  Sending and Retrieving Data from Internet with `URLSession` and the
+#  `Codable` protocol
+
+## Sending and Receiving `Codable` data using `URLSession` and `SwiftUI`
+
+### 
+
