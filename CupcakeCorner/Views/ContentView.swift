a2eda4f86cf36d862c2eaad2cd9db941265a1210
Saverio Negro <saverionegro@gmail.com>
2024-11-12 22:59:09 +0100

    Create an asynchronous task to manage the fetching of data, decode the JSON object into an array of  instances and update the  state property.

--- CupcakeCorner/Views/ContentView.swift
+++ CupcakeCorner/Views/ContentView.swift
@@ -19,6 +19,29 @@ struct ContentView: View {
                 Text(item.collectionName)
             }
         }
+        .task {
+            guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+                print("Invalid URL")
+                return
+            }
+            
+            do {
+                let (dataJSON, response) = try await URLSession(configuration: .default).data(from: url)
+                let decoder = JSONDecoder()
+                
+                
+                guard let data = try? decoder.decode(MusicData.self, from: dataJSON) else {
+                    print("Invalid data.")
+                    return
+                }
+                
+                results = data.results
+                
+            } catch {
+                print("Invalid URL")
+            }
+            
+        }
     }
 }
 
