cd4207ab97063351af869b40c173a7d5e9f0b08a
Saverio Negro <saverionegro@gmail.com>
2024-11-17 15:07:29 +0100

    Create User Interface in `ContentView` to have the user insert their basic order details when ordering cupcakes. Create local `order` property of type `Order` to store the instance of the `Order` class, which is going to be responsible for keeping track of the data the user inserted. The `Order` object is then pass to the next view - `AddressView` - using the `NavigationLink` view.

---
+++ CupcakeCorner/Views/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Saverio Negro on 16/11/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
--- CupcakeCorner/Views/ContentView.swift
+++ CupcakeCorner/Views/ContentView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var order = Order()
+    var extraInfoView: some View {
+        Group {
+            Toggle("Add extra frosting", isOn: $order.extraFrosting)
+            Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        let specialRequestEnabled = Binding {
+            return order.specialRequestEnabled
+        } set: { value in
+            withAnimation {
+                order.specialRequestEnabled = value
+            }
+        }
+
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Type of cake", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("^[\(order.quantity) cake](inflect: true)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: specialRequestEnabled)
+                    if order.specialRequestEnabled {
+                        extraInfoView
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Delivery Details") {
+                        AddressView(order: order)
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
 }
 
